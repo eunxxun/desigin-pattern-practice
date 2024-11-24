33d4ef40619d47d1e38dae55d97afef1d84fd1b1
eunxxun <esun19@kakao.com>
2024-11-24 22:54:29 +0900

    feat: project init commit

---
+++ src/main/java/com/eunsun/designpatternspractice/DesignPatternsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.eunsun.designpatternspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternsPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eunsun/designpatternspractice/DesignPatternsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eunsun.designpatternspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
